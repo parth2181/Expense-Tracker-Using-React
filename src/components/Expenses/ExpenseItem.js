19c8e42e082b50fb3553356e9632ccea6581cb3a
Parth Jadhav <hiparth2001@gmail.com>
2023-08-02 19:32:54 +0530

    added +100rs button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,29 @@
 import React from "react";
+import { useState } from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
 import Cards from "../UI/Cards";
 
 const ExpenseItem = (props) => {
-const clickHandler=()=>{
+  const [title, setTitle] = useState(props.title);
+  const [ add, setAdd] = useState(props.amount);
+  const clickHandler = () => {
+    setTitle("Updated");
+      setAdd(add + 100);
+    console.log(title);
+  };
 
-console.log("clicked delete Button");
-}
- 
   return (
     <Cards className="expense-item">
-      
-        <ExpenseDate date={props.date} />
-        <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-        <button onClick={clickHandler}>Delete Expense</button>
+      <ExpenseDate date={props.date} />
+      <ExpenseDetails
+        amount={add}
+        location={props.location}
+        title={title}
+      />
+      <button onClick={clickHandler}>Delete Expense</button>
+      <button onClick={clickHandler}>Add $100 in Expense</button>
     </Cards>
   );
 };
